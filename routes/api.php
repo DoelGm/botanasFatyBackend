57856bd0f0fd971920125381e482084c84679b6d
Kallolt2030 <ktbernalgallegos@gmail.com>
2025-05-25 15:23:37 -0600

    revision de post

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostController; //post
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;  
 
 Route::post('/register', [AuthController::class, 'register']);
 // Route::apiResource('/categories', CategoryController::class);
