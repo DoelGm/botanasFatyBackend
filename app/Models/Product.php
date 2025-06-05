43e30a2547600beebbf71540ff2f50f8a1bd080f
Kallolt2030 <ktbernalgallegos@gmail.com>
2025-06-05 13:31:53 -0600

    Productos y post

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,12 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $casts = [
-        'imgs' => 'array',
+        'price' => 'decimal:2',
+        'discount' => 'decimal:2',
+        'category_id' => 'integer',
     ];
     protected $fillable = [
         'name',
         'price',
         'description',
+        'category_id',
+        'discount',
     ];
     protected $hidden = [
         'created_at',
@@ -21,5 +25,22 @@ class Product extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
- 
+     protected $appends = ['image_urls']; 
+ // Dentro del modelo Product.php
+    public function getImageUrlsAttribute()
+    {
+    $urls = [];
+
+    for ($i = 1; $i <= 3; $i++) {
+        $filename = "product_{$this->id}_{$i}.webp";
+        $path = storage_path("app/images/{$filename}");
+
+        if (file_exists($path)) {
+            $urls[] = url("/api/image/product/{$this->id}_{$i}");
+        }
+    }
+
+    return $urls;
+}
+
 }
